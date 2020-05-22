a306c3320a3ec96bd5b1ffe648b75280b2f32e89
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-22 18:06:23 +0300

    First Commit

---
+++ app/src/main/java/com/ruth/checkmeout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ruth.checkmeout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
