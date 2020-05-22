b7962688ea954e38fc5eedb339de1d7ad8b9e7fc
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-22 19:23:47 +0300

    Created menu item and header item for the navigation drawer

--- app/src/main/java/com/ruth/checkmeout/MainActivity.java
+++ app/src/main/java/com/ruth/checkmeout/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ruth.checkmeout;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dl=(DrawerLayout)findViewById(R.id.activity_main);
+        t=new ActionBarDrawerToggle(this,dl,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
     }
 }
