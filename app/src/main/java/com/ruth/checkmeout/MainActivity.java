7538801e13044191bd478986b09b7a3b21f764f6
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-23 11:10:10 +0300

    Worked on the navigation drawer and added some items and icons to the menu

--- app/src/main/java/com/ruth/checkmeout/MainActivity.java
+++ app/src/main/java/com/ruth/checkmeout/MainActivity.java
@@ -2,11 +2,16 @@ package com.ruth.checkmeout;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout dl;
@@ -17,13 +22,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         dl=(DrawerLayout)findViewById(R.id.activity_main);
-        t=new ActionBarDrawerToggle(this,dl,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        t=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        t.setDrawerIndicatorEnabled(true);
         dl.addDrawerListener(t);
         t.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        nv = (NavigationView)findViewById(R.id.nv);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                dl.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
