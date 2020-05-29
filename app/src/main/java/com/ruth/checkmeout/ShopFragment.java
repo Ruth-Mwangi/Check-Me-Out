e6a7d414173a96ec688db1972bf6c29c728799f8
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-29 16:52:14 +0300

    Created shopfragment layout file

--- app/src/main/java/com/ruth/checkmeout/ShopFragment.java
+++ app/src/main/java/com/ruth/checkmeout/ShopFragment.java
@@ -8,15 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class ShopFragment extends Fragment {
 
+    @BindView(R.id.btnScan)
+    FloatingActionButton btnScan;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        View view=inflater.inflate(R.layout.fragment_shop, container, false);
+        ButterKnife.bind(this,view);
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+        return view;
     }
 }
