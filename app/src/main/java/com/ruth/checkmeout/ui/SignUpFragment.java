a3129d26425fe6345609ee9ed17906f7c2cf84f8
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-06-05 18:51:32 +0300

    Created the createNewUser method that creates a new user in firebase

--- app/src/main/java/com/ruth/checkmeout/ui/SignUpFragment.java
+++ app/src/main/java/com/ruth/checkmeout/ui/SignUpFragment.java
@@ -8,13 +8,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ruth.checkmeout.R;
 import com.ruth.checkmeout.models.CheckMeOutSearchResponse;
 import com.ruth.checkmeout.networks.CheckMeOutApi;
@@ -41,6 +46,7 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
     Button signUpButton;
     @BindView(R.id.txtGoToLogin)
     TextView txtGoToLogin;
+    @BindView(R.id.signUpConfirmPassword) EditText signUpConfirmPassword;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseAuth mAuth;
     private ProgressDialog mAuthProgressDialog;
@@ -58,8 +64,6 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
         createAuthStateListener();
         createAuthProcessDialog();
 
-
-
         return view;
     }
 
@@ -76,18 +80,12 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v==signUpButton){
-            String name=signUpName.getText().toString();
-            String email=signUpEmail.getText().toString();
-            String password=signUpPassword.getText().toString();
-
-            Bundle bundle=new Bundle();
-            bundle.putString("name",name);
-            bundle.putString("email",email);
-            bundle.putString("password",password);
+            createNewUser();
+        }
+        if(v==txtGoToLogin){
             fragmentClass=LogInFragment.class;
             try {
                 fragment = (Fragment) fragmentClass.newInstance();
-                fragment.setArguments(bundle);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -97,5 +95,44 @@ public class SignUpFragment extends Fragment implements View.OnClickListener{
 
         }
 
+    }
+
+    private void createNewUser() {
+        final String name=signUpName.getText().toString().trim();
+        final String email=signUpEmail.getText().toString().trim();
+        mName= signUpName.getText().toString().trim();
+        String password=signUpPassword.toString().trim();
+        String confirmPassword=signUpConfirmPassword.toString().trim();
+        boolean validName=isValidName(name);
+        boolean validEmail=isValidEmail(email);
+        boolean validPassword=isValidPassword(password,confirmPassword);
+        if(!validEmail||!validName||!validPassword)return;
+        mAuthProgressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Toast.makeText(getContext(),"Authentication Successful",Toast.LENGTH_SHORT).show();
+                            createFirebaseUserProfile(task.getResult().getUser());
+                        }else {
+                            Toast.makeText(getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createFirebaseUserProfile(FirebaseUser user) {
+    }
+
+    private boolean isValidEmail(String email) {
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+    }
+
+    private boolean isValidName(String name) {
     }
 }
